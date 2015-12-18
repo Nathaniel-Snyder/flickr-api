c21147e46af1063beadd17023483c52dafffec3d
Nathaniel-Snyder <nrsnyder8@gmail.com>
2015-12-18 10:59:41 -0600

    Basic Flickr API project

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,8 @@
+class StaticPagesController < ApplicationController
+
+	def index 
+		@user_id = params[:user_id]
+	end
+
+
+end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,12 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+
 # Initialize the Rails application.
 Rails.application.initialize!
+
+require 'flickraw'
+
+
+FlickRaw.api_key=ENV["flickr_key"]
+FlickRaw.shared_secret=ENV["flickr_secret"]
